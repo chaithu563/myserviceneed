ab9142088e10abb5d188d979c523bfe79ea38c2b
chaitanya edara <chaitanya.eedara@gmail.com>
2016-12-17 17:26:27 +0530

    Basic JQuery smart menu Implementation is done. About to  start dynamic smart menu

--- app/app.config.js
+++ app/app.config.js
@@ -1,15 +1,15 @@
-System.register([], function (exports_1, context_1) {
+System.register([], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var MSN_DI_CONFIG;
     return {
-        setters: [],
-        execute: function () {
+        setters:[],
+        execute: function() {
             exports_1("MSN_DI_CONFIG", MSN_DI_CONFIG = {
                 oDataEndPoint: 'http://localhost/MSNAdmin/odata',
                 oDataProvider: 'oData'
             });
         }
-    };
+    }
 });
 //# sourceMappingURL=app.config.js.map
--- app/core/header/header.ts
+++ app/core/header/header.ts
@@ -1,16 +1,23 @@
-﻿import { Component } from '@angular/core';
-
+﻿import { Component,ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+declare var $:JQueryStatic;
 @Component({
     selector: 'header',
     templateUrl: 'app/core/header/header.html',
 		styleUrls: ['app/core/header/header.css'],
     providers: []
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
 
     constructor() {
 
       
+    }
+
+    ngAfterViewInit() {
+        $('#main-menu').smartmenus({
+            subMenusSubOffsetX: 1,
+            subMenusSubOffsetY: -8
+        });
     }
 
 }
