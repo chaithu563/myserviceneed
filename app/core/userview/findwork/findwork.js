504e67e68827042580fb093e0adaccad73bb3550
chaitanya edara <chaitanya.eedara@gmail.com>
2017-01-19 08:33:28 +0530

    need to work on zoom dimensions

--- app/core/userview/findwork/findwork.js
+++ app/core/userview/findwork/findwork.js
@@ -10,7 +10,27 @@ System.register(["@angular/core", "../../../services/msn.service", "../../../ser
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var __moduleName = context_1 && context_1.id;
-    var core_1, msn_service_1, msn_pager_1, router_1, core_2, FindWorkComponent;
+    function getBoundsZoomLevel(bounds, mapDim) {
+        var WORLD_DIM = { height: 256, width: 256 };
+        var ZOOM_MAX = 21;
+        function latRad(lat) {
+            var sin = Math.sin(lat * Math.PI / 180);
+            var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+            return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+        }
+        function zoom(mapPx, worldPx, fraction) {
+            return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+        }
+        var ne = bounds.getNorthEast();
+        var sw = bounds.getSouthWest();
+        var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
+        var lngDiff = ne.lng() - sw.lng();
+        var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+        var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+        var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+        return Math.min(latZoom, lngZoom, ZOOM_MAX);
+    }
+    var core_1, msn_service_1, msn_pager_1, router_1, core_2, FindWorkComponent, _this, newDate, offset, hours;
     return {
         setters: [
             function (core_1_1) {
@@ -54,47 +74,15 @@ System.register(["@angular/core", "../../../services/msn.service", "../../../ser
                             var place = autocomplete.getPlace();
                             _this.servicessearch.latitude = place.geometry.location.lat();
                             _this.servicessearch.longitude = place.geometry.location.lng();
+                            var h = (place.geometry.viewport);
+                            var ne = new google.maps.LatLng(place.geometry.viewport.getNorthEast());
+                            var sw = new google.maps.LatLng(place.geometry.viewport.getSouthWest());
                             //	this.findCity();
                             //this.userAddress = "";
                             console.log(place);
+                            getBoundsZoomLevel(place.geometry.viewport, 'passdim');
                         });
                     });
-                };
-                FindWorkComponent.prototype.findCurrentLocation = function () {
-                    var _this = this;
-                    // Try HTML5 geolocation.
-                    if (navigator.geolocation) {
-                        navigator.geolocation.getCurrentPosition(function (position) {
-                            _this.servicessearch.latitude = position.coords.latitude;
-                            _this.servicessearch.longitude = position.coords.longitude;
-                            //_this.findCity();
-                        }, function () {
-                            alert('error');
-                        });
-                    }
-                    else {
-                        // Browser doesn't support Geolocation
-                        alert('error');
-                    }
-                };
-                FindWorkComponent.prototype.serviceSelected = function (object) {
-                    if (object && object.NAME)
-                        this.servicessearch.serviceid = object.ID;
-                    console.log(object);
-                };
-                FindWorkComponent.prototype.convertUTCDateToLocalDate = function (date) {
-                    var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
-                    var offset = date.getTimezoneOffset() / 60;
-                    var hours = date.getHours();
-                    newDate.setHours(hours - offset);
-                    return newDate;
-                };
-                FindWorkComponent.prototype.onServiceSelected = function (object) {
-                    //if (object && object.NAME)
-                    //	this.router.navigate(['postservice', object.ID]);
-                    // this.router.navigateByUrl('postservice/' + object.ID);
-                    this.selectedService = object.value;
-                    console.log(object);
                 };
                 return FindWorkComponent;
             }());
@@ -109,6 +97,46 @@ System.register(["@angular/core", "../../../services/msn.service", "../../../ser
                 __metadata("design:paramtypes", [msn_service_1.MSNService, msn_pager_1.PagerService, router_1.Router, core_1.NgZone, core_2.MapsAPILoader])
             ], FindWorkComponent);
             exports_1("FindWorkComponent", FindWorkComponent);
+            findCurrentLocation();
+            {
+                _this = this;
+                // Try HTML5 geolocation.
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        _this.servicessearch.latitude = position.coords.latitude;
+                        _this.servicessearch.longitude = position.coords.longitude;
+                        //_this.findCity();
+                    }, function () {
+                        alert('error');
+                    });
+                }
+                else {
+                    // Browser doesn't support Geolocation
+                    alert('error');
+                }
+            }
+            serviceSelected(object);
+            {
+                if (object && object.NAME)
+                    this.servicessearch.serviceid = object.ID;
+                console.log(object);
+            }
+            convertUTCDateToLocalDate(date);
+            {
+                newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+                offset = date.getTimezoneOffset() / 60;
+                hours = date.getHours();
+                newDate.setHours(hours - offset);
+                return newDate;
+            }
+            onServiceSelected(object);
+            {
+                //if (object && object.NAME)
+                //	this.router.navigate(['postservice', object.ID]);
+                // this.router.navigateByUrl('postservice/' + object.ID);
+                this.selectedService = object.value;
+                console.log(object);
+            }
         }
     };
 });
--- app/core/userview/findwork/findwork.ts
+++ app/core/userview/findwork/findwork.ts
@@ -9,102 +9,136 @@ import {MapViewWorkComponent} from './mapviewwork/mapviewwork';
 import { Observable } from 'rxjs/Observable';
 import { AgmCoreModule, MapsAPILoader, NoOpMapsAPILoader, MouseEvent } from 'angular2-google-maps/core';
 @Component({
-	selector: 'findwork',
-	templateUrl: 'app/core/userview/findwork/findwork.html',
-	styleUrls: ['app/core/userview/findwork/findwork.css'],
-	providers: [MSNService, PagerService],
-	directives: []
+    selector: 'findwork',
+    templateUrl: 'app/core/userview/findwork/findwork.html',
+    styleUrls: ['app/core/userview/findwork/findwork.css'],
+    providers: [MSNService, PagerService],
+    directives: []
 })
 export class FindWorkComponent {
-	avilableServices: any;
-	searchString: string;
-	selectedService: any;
-	listview: boolean = false;
-	servicessearch: Observable<any>;
-  //  searchUrl: string = "http://localhost/MSNServiceApi/api/FetchServices?search=:keyword";
-	constructor(private msnService: MSNService, private pagerService: PagerService, private router: Router, private zone: NgZone, private _loader: MapsAPILoader) {
+    avilableServices: any;
+    searchString: string;
+    selectedService: any;
+    listview: boolean = false;
+    servicessearch: Observable<any>;
+    //  searchUrl: string = "http://localhost/MSNServiceApi/api/FetchServices?search=:keyword";
+    constructor(private msnService: MSNService, private pagerService: PagerService, private router: Router, private zone: NgZone, private _loader: MapsAPILoader) {
 
-		this.servicessearch = {};
-		//this.servicessearch.latitude = 16.3066;
-		//this.servicessearch.longitude = 80.43654;
+        this.servicessearch = {};
+        //this.servicessearch.latitude = 16.3066;
+        //this.servicessearch.longitude = 80.43654;
 
-				this.avilableServices = this.msnService.getAvailableServicesURL();
-				this.findCurrentLocation();
-		this.loadAutocomplete();
-
-
-	}
+        this.avilableServices = this.msnService.getAvailableServicesURL();
+        this.findCurrentLocation();
+        this.loadAutocomplete();
 
 
-
-	loadAutocomplete() {
+    }
 
 
 
-		this._loader.load().then(() => {
-			let autocomplete = new google.maps.places.Autocomplete(document.getElementById("address"), {});
-			google.maps.event.addListener(autocomplete, 'place_changed', () => {
-				let place = autocomplete.getPlace();
-				this.servicessearch.latitude = place.geometry.location.lat();
-        this.servicessearch.longitude = place.geometry.location.lng();
-				//	this.findCity();
-								//this.userAddress = "";
-				console.log(place);
-			});
-		});
-
-	}
-
-	findCurrentLocation() {
-		var _this = this;
-		// Try HTML5 geolocation.
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				_this.servicessearch.latitude = position.coords.latitude;
-				_this.servicessearch.longitude = position.coords.longitude;
-
-				//_this.findCity();
-
-			}, function () {
-				alert('error');
-			});
-		} else {
-			// Browser doesn't support Geolocation
-			alert('error');
-		}
-
-	}
-
-	serviceSelected(object) {
-		if (object && object.NAME)
-			this.servicessearch.serviceid = object.ID;
-		console.log(object);
-	}
+    loadAutocomplete() {
 
 
-	 convertUTCDateToLocalDate(date) {
-	var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
 
-	var offset = date.getTimezoneOffset() / 60;
-	var hours = date.getHours();
+        this._loader.load().then(() => {
+            let autocomplete = new google.maps.places.Autocomplete(document.getElementById("address"), {});
+            google.maps.event.addListener(autocomplete, 'place_changed', () => {
+                let place = autocomplete.getPlace();
+                this.servicessearch.latitude = place.geometry.location.lat();
+                this.servicessearch.longitude = place.geometry.location.lng();
+                var h = (place.geometry.viewport);
+                var ne = new google.maps.LatLng(place.geometry.viewport.getNorthEast());
+                var sw = new google.maps.LatLng(place.geometry.viewport.getSouthWest());
+                //	this.findCity();
+                //this.userAddress = "";
+                console.log(place);
+                getBoundsZoomLevel(place.geometry.viewport,'passdim');
+            });
+        });
 
-	newDate.setHours(hours - offset);
+    }
 
-	return newDate;
+    function getBoundsZoomLevel(bounds, mapDim) {
+    var WORLD_DIM = { height: 256, width: 256 };
+    var ZOOM_MAX = 21;
+
+    function latRad(lat) {
+        var sin = Math.sin(lat * Math.PI / 180);
+        var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+        return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+    }
+
+    function zoom(mapPx, worldPx, fraction) {
+        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+    }
+
+    var ne = bounds.getNorthEast();
+    var sw = bounds.getSouthWest();
+
+    var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
+
+    var lngDiff = ne.lng() - sw.lng();
+    var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+
+    var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+    var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+
+    return Math.min(latZoom, lngZoom, ZOOM_MAX);
 }
 
 
 
-	onServiceSelected(object) {
-		//if (object && object.NAME)
-		//	this.router.navigate(['postservice', object.ID]);
-		// this.router.navigateByUrl('postservice/' + object.ID);
-		this.selectedService = object.value;
+findCurrentLocation() {
+    var _this = this;
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            _this.servicessearch.latitude = position.coords.latitude;
+            _this.servicessearch.longitude = position.coords.longitude;
+
+            //_this.findCity();
+
+        }, function () {
+            alert('error');
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        alert('error');
+    }
+
+}
+
+serviceSelected(object) {
+    if (object && object.NAME)
+        this.servicessearch.serviceid = object.ID;
+    console.log(object);
+}
+
+
+convertUTCDateToLocalDate(date) {
+    var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+
+    var offset = date.getTimezoneOffset() / 60;
+    var hours = date.getHours();
+
+    newDate.setHours(hours - offset);
+
+    return newDate;
+}
 
 
 
-		console.log(object);
-	}
+onServiceSelected(object) {
+    //if (object && object.NAME)
+    //	this.router.navigate(['postservice', object.ID]);
+    // this.router.navigateByUrl('postservice/' + object.ID);
+    this.selectedService = object.value;
+
+
+
+    console.log(object);
+}
 
 
 }
