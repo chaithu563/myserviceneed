c00bef84d7940659d05536e0089a7454dbcab4a0
chaitanya edara <chaitanya.eedara@gmail.com>
2017-01-31 07:50:58 +0530

    basic structure for login authentication added

---
+++ MSNService/MSNServiceApi/MSNServiceApi/Models/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSNServiceApi.Models
+{
+    public enum ApplicationTypes
+    {
+        JavaScript = 0,
+        NativeConfidential = 1
+    };
+    
+}
